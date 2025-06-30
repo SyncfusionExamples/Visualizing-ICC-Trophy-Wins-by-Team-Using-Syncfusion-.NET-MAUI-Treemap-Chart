23e26ddeaa6867bac4f89100ef94687510a6e307
Vallarasu <vallarasu.ravichandran@syncfusion.com>
2025-06-30 15:42:05 +0530

    Adding Sample

---
+++ TreeMapChart/TreeMapChart/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TreeMapChart
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TreeMapChart/TreeMapChart/Model/WorldCupStats.cs
@@ -0,0 +1,10 @@
+﻿namespace TreeMapChart
+{
+    public class WorldCupStats
+    {
+        public string? Country { get; set; }
+        public int TotalTitles { get; set; }
+        public string? Category { get; set; }
+        public int Titles { get; set; }
+    }
+}
